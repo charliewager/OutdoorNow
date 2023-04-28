0ccec237332ad6a8b4b2fa2c34a70682d337e8dc
charliewager <charliewager@uvic.ca>
2023-04-28 11:43:25 -0700

    full project

---
+++ outdoor-now/src/helpers/googleAPI/reverseGeocode.js
@@ -0,0 +1,34 @@
+const fetchWrapper = require("../fetchWrapper");
+async function reverseGeocode(req, res){
+    const {lat, long} = JSON.parse(req.body);
+    var output;
+    var latlng = lat + ',' + long;
+
+    // REMOVE BEFORE COMMIT**
+    const key =  'AIzaSyBHTQI0opS0l6tlQzbUgqEljSw_Ieco7tI';
+       const headers = {
+    };
+    const url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latlng + '&key=' + key;
+    try{
+        var address = await fetchWrapper.get(url, headers);
+    } catch(error){
+        res.status(404).json({})
+    }
+    if (!address){
+        res.status(404).json({});
+    } else{
+        for (let obj of address.results){
+            if (obj.types.includes('street_address')){
+                output = obj.formatted_address;
+            }
+        }
+        if (!output){
+            res.status(404).json({})
+        }else{
+            res.status(200).json({output});
+        }
+    }
+    
+}
+
+module.exports = reverseGeocode
